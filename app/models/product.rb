be2068c2c78ac4ee27e61ccd99f783ad37846ac8
TheAlacrity <brinker.jeff@gmail.com>
2019-06-04 10:30:10 -0500

    added gems for js functionality; updated product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: {maximum: 254}
   
   validates :price, presence: true
-  validates :price, numericality: {less_than: 100_000_000, greater_than: 0}
+  validates :price, numericality: {greater_than: 0}
 
   validates :description, presence: true
 
